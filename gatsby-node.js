9f4df1afa8d91904b55ee9efcb8eaca259b0fa3f
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-07-29 11:53:08 -0400

    add graphql and templates

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,28 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path')
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const { data } = await graphql(`
+    query {
+      articles: allDatoCmsArticle {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+  data.articles.edges.forEach(({ node }) => {
+    createPage({
+      path: `article/${node.slug}`,
+      component: path.resolve('./src/templates/article.js'),
+      context: {
+        slug: node.slug,
+      },
+    })
+  })
+}
---
+++ src/templates/article.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const article = () => {
+  return (
+    <div>
+      <h1>Hello from Article</h1>
+    </div>
+  )
+}
+
+export default article
