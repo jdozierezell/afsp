a2a773fd2ea9bf7f4275b910ee9360b16b78f69d
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-09-20 06:58:14 -0400

    starting work on quilt

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
 function App() {
-  return <h1>Hello from Gatsby</h1>
+  return (
+    <Layout>
+      <SEO title="American Foundation for Suicide Prevention" />
+      <h1>Hello from Gatsby</h1>
+    </Layout>
+  )
 }
 
 export default App
---
+++ src/pages/quilt.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const Quilt = () => (
+  <Layout>
+    <SEO title="Digital Memory Quilt" />
+    <h1>FOO</h1>
+  </Layout>
+)
+
+export default Quilt
