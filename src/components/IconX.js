20f02d5f3d9b82673b3e7aca0fc3b119c3f38a07
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-10-01 10:46:19 -0400

    trying to implement more dynamic window size based loading

---
+++ src/components/IconX.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { css } from '@emotion/core'
+
+import { styles } from '../css/css'
+
+const CaretIcon = ({ color }) => {
+	return (
+		<svg viewBox="0 0 15.755 15.75">
+			<path
+				id="Icon_ionic-ios-close"
+				data-name="Icon ionic-ios-close"
+				d="M21.029,19.164l5.627-5.627a1.318,1.318,0,1,0-1.865-1.865L19.164,17.3l-5.627-5.627a1.318,1.318,0,1,0-1.865,1.865L17.3,19.164l-5.627,5.627a1.318,1.318,0,1,0,1.865,1.865l5.627-5.627,5.627,5.627a1.318,1.318,0,0,0,1.865-1.865Z"
+				transform="translate(-11.285 -11.289)"
+			/>
+		</svg>
+	)
+}
+
+export default CaretIcon
---
+++ src/components/SearchBar.js
@@ -0,0 +1,69 @@
+import React, { useRef, useEffect, useState } from 'react'
+import { css } from '@emotion/core'
+
+import IconX from './IconX'
+
+import { styles } from '../css/css'
+
+const searchCSS = css`
+	background-color: ${styles.colors.blue};
+	padding: ${styles.scale.px50} ${styles.scale.px24} ${styles.scale.px35};
+	position: relative;
+	input {
+		width: 100%;
+		border-radius: 5px;
+		border: none;
+		padding: ${styles.scale.px14};
+		font-family: ${styles.fonts.avenirRegular};
+		font-size: ${styles.scale.px17};
+	}
+	button {
+		position: absolute;
+		right: ${styles.scale.px36};
+		top: ${styles.scale.px62};
+		background-color: transparent;
+		border: none;
+		padding: 0;
+		width: ${styles.scale.px24};
+		height: ${styles.scale.px36};
+	}
+`
+
+const SearchBar = () => {
+	const [searchValue, setSearchValue] = useState('')
+	const [showX, setShowX] = useState(false)
+	const searchRef = useRef(null)
+
+	const showXFunc = () => {
+		if (searchRef.current.value !== '') {
+			setShowX(true)
+		} else {
+			setShowX(false)
+		}
+		setSearchValue(searchRef.current.value)
+	}
+
+	return (
+		<div css={searchCSS}>
+			<input
+				type="text"
+				placeholder="Search..."
+				value={searchValue}
+				ref={searchRef}
+				onChange={() => showXFunc()}
+			/>
+			{showX && (
+				<button
+					onClick={() => {
+						setSearchValue('')
+						setShowX(false)
+					}}
+				>
+					<IconX />
+				</button>
+			)}
+		</div>
+	)
+}
+
+export default SearchBar
