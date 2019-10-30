253957b19cfc4aa274469debe861291e72b49498
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-10-29 22:49:14 -0400

    fixing 404 null variable condition v2

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,11 @@ import SEO from '../components/SEO'
 import { styles } from '../css/css'
 
 const NotFoundPage = () => (
+  const meta = {
+    tags: [{tagName: 'title', content: '404: Page not found'}]
+  }
 	<Layout logo={styles.logo.mobileLightDesktopLight}>
-		<SEO title="404: Page not found" />
+		<SEO meta={meta} />
 		<h1>404 NOT FOUND</h1>
 		<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
 	</Layout>
