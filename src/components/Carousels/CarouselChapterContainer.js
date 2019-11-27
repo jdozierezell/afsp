681eee757db00e31b05fb76761b833e889d040be
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2019-11-27 15:42:18 -0500

    trying to isolate jpg error v2

--- src/components/Carousels/CarouselChapterContainer.js
+++ src/components/Carousels/CarouselChapterContainer.js
@@ -73,33 +73,7 @@ const CarouselChapterContainer = ({ location }) => {
 	const data = useStaticQuery(graphql`
 		query {
 			chapters: allDatoCmsChapterHomePage {
-				edges {
-					node {
-						title
-						slug
-						chapterZipCodeSet {
-							zipCode
-						}
-						heroImage {
-							fluid(
-								maxWidth: 600
-								imgixParams: {
-									fm: "jpg"
-									fit: "crop"
-									crop: "faces"
-									w: "600"
-									ar: 1.67
-								}
-							) {
-								...GatsbyDatoCmsFluid_tracedSVG
-							}
-						}
-						staffName
-						staffTitle
-						staffEmail
-						staffPhone
-					}
-				}
+				...ChapterSearch
 			}
 		}
 	`)
--- src/fragments/ChapterSearch.js
+++ src/fragments/ChapterSearch.js
@@ -1,33 +1,33 @@
-// import { graphql } from 'gatsby'
+import { graphql } from 'gatsby'
 
-// export const ChapterSearch = graphql`
-// 	fragment ChapterSearch on DatoCmsChapterHomePageConnection {
-// 		edges {
-// 			node {
-// 				title
-// 				slug
-// 				chapterZipCodeSet {
-// 					zipCode
-// 				}
-// 				heroImage {
-// 					fluid(
-// 						maxWidth: 600
-// 						imgixParams: {
-// 							fm: "jpg"
-// 							fit: "crop"
-// 							crop: "faces"
-// 							w: "600"
-// 							ar: 1.67
-// 						}
-// 					) {
-// 						...GatsbyDatoCmsFluid_tracedSVG
-// 					}
-// 				}
-// 				staffName
-// 				staffTitle
-// 				staffEmail
-// 				staffPhone
-// 			}
-// 		}
-// 	}
-// `
+export const ChapterSearch = graphql`
+	fragment ChapterSearch on DatoCmsChapterHomePageConnection {
+		edges {
+			node {
+				title
+				slug
+				chapterZipCodeSet {
+					zipCode
+				}
+				heroImage {
+					fluid(
+						maxWidth: 600
+						imgixParams: {
+							fm: "jpg"
+							fit: "crop"
+							crop: "faces"
+							w: "600"
+							ar: 1.67
+						}
+					) {
+						...GatsbyDatoCmsFluid_tracedSVG
+					}
+				}
+				staffName
+				staffTitle
+				staffEmail
+				staffPhone
+			}
+		}
+	}
+`
