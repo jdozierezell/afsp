2389d1eef5a9f75c7180271b44348f1d7ef52160
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-01-24 04:41:06 -0700

    removing localhost from gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,8 @@ module.exports = {
 		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
 		author: `@gatsbyjs`,
 		siteUrl:
-			process.env.NODE_ENV === 'development'
-				? `http://localhost:8000`
-				: `https://reverent-payne-b0d24a.netlify.com`,
+			process.env.NODE_ENV ===
+			`https://reverent-payne-b0d24a.netlify.com`,
 	},
 	plugins: [
 		`gatsby-plugin-emotion`,
