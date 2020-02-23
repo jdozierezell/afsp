ccd6e6507daada6bf743c0074109d429dbf6251d
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-02-23 17:22:42 -0500

    rerouting 404 page to search

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,23 +1,15 @@
-import React from 'react'
+import { useEffect } from 'react'
+import { navigate } from 'gatsby'
 
-import Layout from '../components/Layout'
-import { HelmetDatoCms } from 'gatsby-source-datocms'
-
-import { styles } from '../css/css'
-
-const NotFoundPage = () => {
-	const meta = {
-		seoMetaTags: {
-			tags: [{ tagName: 'title', content: '404: Page not found' }],
-		},
-	}
-	return (
-		<Layout theme={styles.logo.mobileLightDesktopLight}>
-			<HelmetDatoCms seo={meta.seoMetaTags} />
-			<h1>404 NOT FOUND</h1>
-			<p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-		</Layout>
-	)
+const Redirect404 = () => {
+	const pathArray = window.location.pathname.split('/')
+	pathArray.shift()
+	const pathString = pathArray.join('+')
+	console.log(pathString)
+	useEffect(() => {
+		navigate(`/search-results/?query=${pathString}`)
+	}, [])
+	return null
 }
 
-export default NotFoundPage
+export default Redirect404
