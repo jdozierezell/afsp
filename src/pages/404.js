e0b1029d501c1465cf1ff5bb7cd6565ba77ead29
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-03-07 15:59:35 -0500

    testing for window on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,13 +2,15 @@ import { useEffect } from 'react'
 import { navigate } from 'gatsby'
 
 const Redirect404 = () => {
-	const pathArray = window.location.pathname.split('/')
-	pathArray.shift()
-	const pathString = pathArray.join('+')
-	console.log(pathString)
-	useEffect(() => {
-		navigate(`/search-results/?query=${pathString}`)
-	}, [])
+	if (typeof window !== `undefined`) {
+		const pathArray = window.location.pathname.split('/')
+		pathArray.shift()
+		const pathString = pathArray.join('+')
+		console.log(pathString)
+		useEffect(() => {
+			navigate(`/search-results/?query=${pathString}`)
+		}, [])
+	}
 	return null
 }
 
