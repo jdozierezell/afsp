924f8f68f6ffa6f0d7ce0994412c11f529687663
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-03-13 09:11:47 -0400

    added chapter social links

---
+++ src/components/Social/ChapterSocials.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { css } from '@emotion/core'
+
+import IconFacebook from '../SVGs/IconFacebook'
+import IconTwitter from '../SVGs/IconTwitter'
+import IconInstagram from '../SVGs/IconInstagram'
+
+import { styles } from '../../css/css'
+
+const chapterSocialsCSS = css`
+	background-color: ${styles.colors.darkBlue};
+	div {
+		width: 400px;
+		padding: ${styles.scale.px24};
+		margin: 0 auto;
+		display: flex;
+		justify-content: space-around;
+	}
+	a {
+		line-height: 0;
+	}
+`
+
+const iconCSS = css`
+	width: ${styles.scale.px36};
+`
+
+const ChapterSocials = ({ socialAccounts }) => {
+	return (
+		<div css={chapterSocialsCSS}>
+			<div>
+				{socialAccounts.map((account, index) => {
+					if (account.socialPlatform === 'Facebook') {
+						return (
+							<a
+								key={index}
+								href={`https://facebook.com/${account.accountHandle}`}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<IconFacebook
+									color={styles.colors.white}
+									iconCSS={iconCSS}
+								/>
+							</a>
+						)
+					} else if (account.socialPlatform === 'Twitter') {
+						return (
+							<a
+								key={index}
+								href={`https://twitter.com/${account.accountHandle}`}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<IconTwitter
+									color={styles.colors.white}
+									iconCSS={iconCSS}
+								/>
+							</a>
+						)
+					} else if (account.socialPlatform === 'Instagram') {
+						return (
+							<a
+								key={index}
+								href={`https://instagram.com/${account.accountHandle}`}
+								target="_blank"
+								rel="noopener noreferrer"
+							>
+								<IconInstagram
+									color={styles.colors.white}
+									iconCSS={iconCSS}
+								/>
+							</a>
+						)
+					} else {
+						return null
+					}
+				})}
+			</div>
+		</div>
+	)
+}
+
+export default ChapterSocials
