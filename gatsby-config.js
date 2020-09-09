275553d36a02f2abe53c691fb1e261f6bb16392b
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-09-09 14:13:13 -0400

    removed console log from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,6 @@
 require('dotenv').config({
 	path: `.env.${process.env.NODE_ENV}`,
 })
-console.log(process.env.NODE_ENV)
-console.log(process.env.PREVIEW_MODE)
 module.exports = {
 	siteMetadata: {
 		title: `Gatsby Default Starter`,
