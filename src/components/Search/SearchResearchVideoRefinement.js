c9ba85526b9bebe185d12377c577cb7bc426bcb6
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-09-14 13:06:08 -0400

    redeveloping video search

---
+++ src/components/Search/SearchResearchVideoRefinement.js
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from 'react'
+import { connectRefinementList } from 'react-instantsearch-dom'
+import Select from 'react-select'
+import { css } from '@emotion/core'
+
+import { styles } from '../../css/css'
+
+const refinementListCSS = css`
+	ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+	button {
+		background: none;
+		border: none;
+		cursor: pointer;
+		border-radius: ${styles.scale.px5};
+		padding: ${styles.scale.px7};
+	}
+	span {
+		background-color: ${styles.colors.lightGray};
+		padding: ${styles.scale.px5} ${styles.scale.px5} ${styles.scale.px2};
+		border-radius: ${styles.scale.px5};
+	}
+`
+
+const selectCSS = css`
+	margin-bottom: ${styles.scale.px36};
+	.react-select__control {
+		border: 2px solid ${styles.colors.darkGray};
+		border-radius: 5px;
+	}
+	.react-select__input {
+		max-height: ${styles.scale.px42};
+	}
+`
+
+const RefinementList = ({
+	items,
+	attribute,
+	displayAttribute,
+	searchState,
+	handleSearchChange,
+}) => {
+	const [options, setOptions] = useState([])
+	const [updateDropdown, setUpdateDropdown] = useState(false)
+	useEffect(() => {
+		setOptions([])
+		items.forEach(item => {
+			setOptions(options => [
+				...options,
+				{ value: item.value[0], label: item.label },
+			])
+		})
+		setUpdateDropdown(false)
+	}, [items, updateDropdown])
+	return (
+		<>
+			<div css={refinementListCSS}>
+				<h3>{displayAttribute}</h3>
+				<Select
+					css={selectCSS}
+					id="meetingCountry"
+					className="react-select"
+					classNamePrefix="react-select"
+					value={'test'}
+					options={options}
+					onChange={e => {
+						handleSearchChange({
+							target: {
+								value: [e.value],
+								attribute: attribute,
+							},
+						})
+						setUpdateDropdown(true)
+					}}
+				/>
+				{/* <ul id={`grant-${attribute}`}>
+					{items.map(item => (
+						<li key={item.label}>
+							<button
+								style={{
+									border: item.isRefined
+										? `${styles.colors.blue} ${styles.scale.px2} solid`
+										: '',
+								}}
+								onClick={() =>
+									handleSearchChange({
+										target: {
+											value: item.value,
+											attribute: attribute,
+										},
+									})
+								}
+							>
+								{item.label} <span>{item.count}</span>
+							</button>
+						</li>
+					))}
+				</ul> */}
+			</div>
+		</>
+	)
+}
+
+const SearchResearchVideoRefinement = connectRefinementList(RefinementList)
+
+export default SearchResearchVideoRefinement
---
+++ src/components/Search/SearchResearchVideos.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import { css } from '@emotion/core'
+import algoliasearch from 'algoliasearch/lite'
+import { InstantSearch, SearchBox } from 'react-instantsearch-dom'
+
+import SearchHits from '../../components/Search/SearchHits'
+import SearchResearchVideoRefinement from '../../components/Search/SearchResearchVideoRefinement'
+
+import { styles } from '../../css/css'
+
+const searchClient = algoliasearch(
+	'BONWJFMMRS',
+	'dc6a5a451c85739a43419955d7a505c1'
+)
+
+const searchDetailCSS = css`
+	/* max-width: 623px; */
+	margin: ${styles.scale.px50} ${styles.scale.px24};
+	@media (min-width: ${styles.screens.mobile}px) {
+		margin: ${styles.scale.px80} ${styles.scale.px50};
+	}
+`
+
+const searchBoxCSS = css`
+	position: relative;
+	input {
+		width: 100%;
+		padding-top: 4px;
+	}
+	button {
+		position: absolute;
+		right: ${styles.scale.px24};
+		top: ${styles.scale.px16};
+		background-color: transparent;
+		border: none;
+		padding: 0;
+		width: ${styles.scale.px24};
+		height: ${styles.scale.px36};
+		cursor: pointer;
+	}
+	svg {
+		width: 20px;
+		height: 20px;
+	}
+	.ais-SearchBox-reset {
+		display: none;
+	}
+`
+
+const indexWrapperCSS = css`
+	width: 100%;
+	@media (min-width: ${styles.screens.navigation}px) {
+		display: grid;
+		grid-template-columns: 400px 1fr;
+	}
+	h2 {
+		margin: ${styles.scale.px36} 0 ${styles.scale.px24};
+	}
+`
+
+const refinementCSS = css`
+	margin-bottom: ${styles.scale.px36};
+	border-bottom: 1px solid ${styles.colors.darkGray};
+	@media (min-width: ${styles.screens.tablet}px) {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+	}
+	@media (min-width: ${styles.screens.navigation}px) {
+		display: block;
+		border-bottom: none;
+	}
+`
+
+const SearchGrants = ({
+	searchState,
+	handleSearchChange,
+	indexName,
+	placeholder,
+	refinements,
+}) => {
+	return (
+		<div css={searchDetailCSS}>
+			<InstantSearch
+				indexName={indexName}
+				searchClient={searchClient}
+				searchState={searchState}
+			>
+				{/* <SearchBox
+					css={searchBoxCSS}
+					onChange={handleSearchChange}
+					translations={{
+						placeholder: placeholder,
+					}}
+				/> */}
+				<div css={indexWrapperCSS}>
+					<div css={refinementCSS}>
+						{refinements.map(refinement => (
+							<SearchResearchVideoRefinement
+								attribute={refinement.attribute}
+								displayAttribute={refinement.displayAttribute}
+								searchState={searchState}
+								handleSearchChange={handleSearchChange}
+								searchable
+							/>
+						))}
+					</div>
+					<div>
+						<SearchHits indexName={indexName} />
+					</div>
+				</div>
+			</InstantSearch>
+		</div>
+	)
+}
+
+export default SearchGrants
