4b64cfd3bdd0b2d3a6e568ed20e9908da89f62be
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-10-15 14:22:44 -0400

    removed mobilecontainer from card container

--- src/components/Cards/CardContainer.js
+++ src/components/Cards/CardContainer.js
@@ -1,12 +1,10 @@
 import React from 'react'
 
-import CardMobileContainer from './CardMobileContainer'
 import CardDesktopContainer from './CardDesktopContainer'
 
 const CardContainer = ({ cards, heading }) => {
 	return (
 		<section>
-			{/* <CardMobileContainer cards={cards} heading={heading} /> */}
 			<CardDesktopContainer cards={cards} heading={heading} />
 		</section>
 	)
