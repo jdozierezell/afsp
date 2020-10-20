c15275d85a5afb500ac0c2d1008ccdf0943f7b01
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-10-20 16:59:18 -0400

    converted to programs calendar

--- src/utils/programList.js
+++ src/utils/programList.js
@@ -1,17 +1,13 @@
 const chapterList = [
 	{
-		value: 'Suicide Bereavement Clinician Trainings',
-		label: 'Suicide Bereavement Clinician Trainings',
-	},
-	{
-		value: 'Suicide Bereavement Facilitator Trainings',
-		label: 'Suicide Bereavement Facilitator Trainings',
-	},
-	{
 		value: 'Attachment-Based Family Therapy Workshops',
 		label: 'Attachment-Based Family Therapy Workshops',
 	},
+	{ value: "It's Real", label: "It's Real" },
 	{ value: 'Mental Health First Aid', label: 'Mental Health First Aid' },
+	{ value: 'More Than Sad', label: 'More Than Sad' },
+	{ value: 'Research Connections', label: 'Research Connections' },
+	{ value: 'SafeTalk', label: 'SafeTalk' },
 	{
 		value: 'Stronger Communities: LGBTQ Suicide Prevention ',
 		label: 'Stronger Communities: LGBTQ Suicide Prevention ',
@@ -20,14 +16,15 @@ const chapterList = [
 		value: 'Stronger Communities: Counseling Veterans at Risk for Suicide ',
 		label: 'Stronger Communities: Counseling Veterans at Risk for Suicide ',
 	},
+	{
+		value: 'Suicide Bereavement Clinician Trainings',
+		label: 'Suicide Bereavement Clinician Trainings',
+	},
+	{
+		value: 'Suicide Bereavement Facilitator Trainings',
+		label: 'Suicide Bereavement Facilitator Trainings',
+	},
 	{ value: 'Talk Saves Lives', label: 'Talk Saves Lives' },
-	{ value: 'Walk Kickoff Fundraisers', label: 'Walk Kickoff Fundraisers' },
-	{ value: 'Volunteer Orientation', label: 'Volunteer Orientation' },
-	{ value: 'Research Connections', label: 'Research Connections' },
-	{ value: 'SafeTalk', label: 'SafeTalk' },
-	{ value: "It's Real", label: "It's Real" },
-	{ value: 'More Than Sad', label: 'More Than Sad' },
-	{ value: 'Other (not listed) ', label: 'Other (not listed) ' },
 ]
 
 export default chapterList
