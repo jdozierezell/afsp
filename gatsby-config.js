1c7c7b3a9bbf631b9a0af81ab4b8511b40c258bb
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-10-28 08:55:19 -0400

    added sentry monitoring

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,14 @@ module.exports = {
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-netlify`,
 		// `gatsby-plugin-preact`,
+		{
+			resolve: '@sentry/gatsby',
+			options: {
+				dsn:
+					'https://716ad2d069ab4105a9dcd2226a66f587@o468082.ingest.sentry.io/5496226',
+				sampleRate: 0.7,
+			},
+		},
 		// {
 		// 	resolve: `gatsby-plugin-loadable-components-ssr`,
 		// 	options: {
