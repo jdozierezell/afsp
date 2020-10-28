3a558b39f3df9ac743ce55895417afa2a3294e88
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-10-28 09:25:13 -0400

    added option to sentry

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,8 @@ module.exports = {
 			options: {
 				dsn:
 					'https://716ad2d069ab4105a9dcd2226a66f587@o468082.ingest.sentry.io/5496226',
-				sampleRate: 0.7,
+				sampleRate: 1,
+				tracesSampleRate: 1,
 			},
 		},
 		// {
