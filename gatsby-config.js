562847d986228f3d62c1a426f315a78f387d31ea
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2020-12-02 15:28:25 -0500

    added stage task to package and updated config info

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ require('dotenv').config({
 })
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@gatsbyjs`,
+		title: `American Foundation for Suicide Prevention`,
+		description: `Save Lives and Bring Hope to Those Affected by Suicide`,
+		author: `@jdozierezell`,
 		siteUrl:
 			process.env.NODE_ENV === 'development'
 				? `http://localhost:8000`
