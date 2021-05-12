dc0b1b90d648e2bc686fb7bdd089131c27b66e51
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2021-05-12 11:29:29 -0400

    removed param plugin for testing

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
 		`gatsby-plugin-image`,
 		`gatsby-plugin-sharp`,
 		`gatsby-transformer-sharp`,
-		`gatsby-plugin-use-query-params`,
+		// `gatsby-plugin-use-query-params`,
 		{
 			resolve: `gatsby-plugin-netlify`,
 			options: {
