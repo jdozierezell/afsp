1951661a49aa83718f49e997bb5748c3ee5404d0
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2021-09-21 09:26:08 -0400

    completed calendar, moving to staging v2

---
+++ src/components/Calendar/CalendarEventModal.js
@@ -0,0 +1,125 @@
+import React, { useState } from 'react'
+import Modal from 'react-modal'
+import { css, ClassNames } from '@emotion/react'
+
+import IconX from '../SVGs/IconX'
+
+import { styles } from '../../css/css'
+
+Modal.setAppElement(`#___gatsby`)
+
+const insideModalCSS = css`
+	h2 {
+		font-size: ${styles.scale.px24} !important;
+		text-align: center;
+	}
+`
+
+const modalEventCSS = css`
+	background-color: ${styles.colors.blue};
+	border-radius: ${styles.scale.px5};
+	padding: ${styles.scale.px5};
+	margin-bottom: ${styles.scale.px7};
+	cursor: pointer;
+	a {
+		color: ${styles.colors.white};
+		text-decoration: none;
+		:hover {
+			color: ${styles.colors.white};
+			text-decoration: underline;
+			font-family: ${styles.fonts.avenirRegular};
+		}
+	}
+`
+
+const xCSS = css`
+	position: absolute;
+	right: ${styles.scale.px25};
+	top: ${styles.scale.px25};
+	width: ${styles.scale.px25};
+	cursor: pointer;
+	background: none;
+	margin: 0;
+	padding: 0;
+	outline: none;
+	border: none;
+`
+
+const CalendarEventModal = ({
+	modalIsOpen,
+	closeModal,
+	focusedEvents,
+	selectedDate,
+}) => {
+	console.log(selectedDate)
+	console.log(focusedEvents)
+	return (
+		<ClassNames>
+			{({ css, cx }) => (
+				<Modal
+					isOpen={modalIsOpen}
+					onRequestClose={closeModal}
+					onAfterOpen={() => {
+						document.body.style.top = `-${window.scrollY}px`
+						document.body.style.width = '100vw'
+						document.body.style.position = 'fixed'
+					}}
+					onAfterClose={() => {
+						const scrollY = document.body.style.top
+						document.body.style.position = ''
+						document.body.style.top = ''
+						window.scrollTo(0, parseInt(scrollY || '0') * -1)
+					}}
+					overlayClassName={css`
+						position: fixed;
+						inset: 0px;
+						background-color: hsla(0, 0%, 14.9%, 0.7);
+						z-index: 9999;
+						overflow: scroll;
+						@media (min-width: ${styles.screens.tablet}px) {
+							display: grid;
+							justify-items: center;
+							align-items: center;
+						}
+					`}
+					className={css`
+						position: relative;
+						padding: ${styles.scale.px44} ${styles.scale.px24};
+						background: hsla(0, 0%, 91.8%, 1);
+						overflow: scroll;
+						@media (min-width: ${styles.screens.tablet}px) {
+							width: 50vw;
+							max-height: 90vh;
+							min-width: 768px;
+							padding: ${styles.scale.px24} ${styles.scale.px64};
+							border-radius: ${styles.scale.px5};
+						}
+					`}
+				>
+					<div css={insideModalCSS}>
+						<button
+							css={css`
+								background: transparent;
+								border: none;
+								outline: none;
+							`}
+							onClick={closeModal}
+						>
+							<IconX iconCSS={xCSS}></IconX>
+						</button>
+						<h2>Events for {selectedDate}</h2>
+						{focusedEvents.map(event => {
+							return (
+								<p css={modalEventCSS}>
+									<a>{event.title}</a>
+								</p>
+							)
+						})}
+					</div>
+				</Modal>
+			)}
+		</ClassNames>
+	)
+}
+
+export default CalendarEventModal
