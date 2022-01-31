9ddbf6bf4d5c23eb42812ddc99c4e932aedddf90
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2022-01-31 15:25:56 -0500

    completed reformatting grid layout

--- src/components/Content/ContentGrid.js
+++ src/components/Content/ContentGrid.js
@@ -62,17 +62,51 @@ const ContentGrid = ({ hideHeader, table }) => {
 	let gridItems = []
 	if (!hideHeader) {
 		tableJSON.columns.forEach(column => {
-			columnContents.push({ isColumn: true, value: column })
+			columnContents.push({
+				isColumn: true,
+				value: column,
+				display: 'desktop',
+			})
 		})
 		gridItems.push(columnContents)
 	}
 	tableJSON.data.forEach(data => {
 		let rowContents = []
-		tableJSON.columns.forEach(column => {
-			if (data.hasOwnProperty(column)) {
-				rowContents.push({ isColumn: false, value: data[column] })
-			}
-		})
+		if (hideHeader) {
+			tableJSON.columns.forEach((column, index) => {
+				if (data.hasOwnProperty(column)) {
+					rowContents.push({
+						isColumn: false,
+						value: data[column],
+						hasLastValue: data[column] !== '' ? true : false,
+					})
+				}
+				if (rowContents.length > 1 && data[column] !== '') {
+					rowContents[index - 1].hasLastValue = false
+				}
+			})
+		} else if (!hideHeader) {
+			tableJSON.columns.forEach((column, index) => {
+				if (data.hasOwnProperty(column)) {
+					rowContents.push({
+						isColumn: true,
+						value: column,
+						display: 'mobile',
+						hasLastValue: data[column] !== '' ? true : false,
+					})
+					rowContents.push({
+						isColumn: false,
+						value: data[column],
+						hasLastValue: data[column] !== '' ? true : false,
+					})
+				}
+				if (rowContents.length > 2 && data[column] !== '') {
+					rowContents[rowContents.length - 3].hasLastValue = false
+					rowContents[rowContents.length - 4].hasLastValue = false
+				}
+			})
+		}
+		console.log(rowContents)
 		dataContents.push(rowContents)
 	})
 	gridItems.push(...dataContents)
@@ -80,6 +114,8 @@ const ContentGrid = ({ hideHeader, table }) => {
 
 	const gridCssMobile = css`
 		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		padding: ${styles.scale.px24} 0;
 		@media (min-width: ${styles.screens.tablet}px) {
 			display: none;
 		}
@@ -88,58 +124,131 @@ const ContentGrid = ({ hideHeader, table }) => {
 	const gridCssDesktop = css`
 		display: none;
 		@media (min-width: ${styles.screens.tablet}px) {
+			background-color: ${styles.colors.white};
 			display: grid;
 			grid-template-columns: repeat(${tableJSON.columns.length}, 1fr);
-			width: calc(100vw - (${styles.scale.px50} * 2));
+			padding: ${styles.scale.px24} 0;
 			position: relative;
+			width: calc(100vw - (${styles.scale.px50} * 2));
 			z-index: 500;
-			background-color: ${styles.colors.white};
 		}
 	`
 
 	const columnCss = css`
-		background-color: ${styles.colors.green};
-		border: 1px solid ${styles.colors.white};
 		font-family: ${styles.fonts.avenirDemi};
 		font-size: ${styles.scale.px16};
 		margin: 0;
 		padding: ${styles.scale.px12};
+		@media (max-width: ${styles.screens.tablet - 1}px) {
+			background-color: hsla(156, 69%, 50.6%, 0.15);
+			border-bottom: 1px solid ${styles.colors.darkGray};
+			grid-column: 1;
+			&[data-grid~='desktop'] {
+				display: none;
+			}
+			&[data-grid*='-0'] {
+				display: none;
+			}
+		}
+		@media (min-width: ${styles.screens.tablet}px) {
+			background-color: ${styles.colors.green};
+			border: 1px solid ${styles.colors.white};
+			&[data-grid~='mobile'] {
+				display: none;
+			}
+		}
+		&[data-grid~='last'] {
+			@media (max-width: ${styles.screens.tablet - 1}px) {
+				border-bottom: none;
+			}
+		}
 	`
 
 	const rowCss = css`
 		border-bottom: 1px solid ${styles.colors.darkGray};
-		border-left: 1px solid ${styles.colors.darkGray};
 		font-family: ${styles.fonts.avenirRegular};
 		margin: 0;
 		padding: ${styles.scale.px12};
-		&[data-row-number='0'] {
-			border-top: 1px solid ${styles.colors.darkGray};
+		@media (max-width: ${styles.screens.tablet - 1}px) {
+			grid-column: 2 / 3;
 		}
-		&[data-column-number='0'] {
+		@media (min-width: ${styles.screens.tablet}px) {
+			border-left: 1px solid ${styles.colors.darkGray};
+			&[data-row-number='0'] {
+				border-top: 1px solid ${styles.colors.darkGray};
+			}
+		}
+		&[data-grid*='-0 hide-header'] {
 			border-left: none;
 			background-color: hsla(156, 69%, 50.6%, 0.15);
 			font-family: ${styles.fonts.avenirBold};
+			@media (max-width: ${styles.screens.tablet - 1}px) {
+				background-color: ${styles.colors.green};
+				border-bottom: none;
+				grid-column: 1 / 3;
+			}
+		}
+		&[data-grid*='hide-header row 0-'] {
+			@media (min-width: ${styles.screens.tablet}px) {
+				border-top: 1px solid ${styles.colors.darkGray};
+			}
+		}
+		&[data-grid*='-1 show-header'] {
+			border-left: none;
+			background-color: hsla(156, 69%, 50.6%, 0.15);
+			font-family: ${styles.fonts.avenirBold};
+			@media (max-width: ${styles.screens.tablet - 1}px) {
+				background-color: ${styles.colors.green};
+				border-bottom: none;
+				grid-column: 1 / 3;
+			}
+		}
+		&[data-grid~='hide-header'] {
+			@media (max-width: ${styles.screens.tablet - 1}px) {
+				grid-column: 1 / 3;
+			}
+		}
+		&[data-grid~='last'] {
+			@media (max-width: ${styles.screens.tablet - 1}px) {
+				border-bottom: none;
+			}
 		}
 	`
-	console.log(gridItems)
+
 	return (
-		<div>
+		<>
 			<div css={gridCssMobile}>
 				{gridItems.map((items, rowIndex) => {
 					return items.map((item, columnIndex) => {
-						return (
-							<div
-								key={rowIndex + columnIndex}
-								data-rows={tableJSON.data.length}
-								data-row-number={rowIndex}
-								data-columns={tableJSON.columns.length}
-								data-column-number={columnIndex}
-								css={
-									item.isColumn === true ? columnCss : rowCss
-								}
-								dangerouslySetInnerHTML={{ __html: item.value }}
-							></div>
-						)
+						if (item.value !== '') {
+							return (
+								<div
+									css={
+										item.isColumn === true
+											? columnCss
+											: rowCss
+									}
+									dangerouslySetInnerHTML={{
+										__html: item.value,
+									}}
+									data-grid={`${
+										hideHeader
+											? 'hide-header'
+											: 'show-header'
+									} ${
+										item.isColumn ? 'column' : 'row'
+									} ${rowIndex}-${columnIndex} ${
+										hideHeader
+											? 'hide-header'
+											: 'show-header'
+									} ${item.display ? item.display : ''} ${
+										item.hasLastValue === true ? 'last' : ''
+									}`}
+									key={rowIndex + columnIndex}
+								></div>
+							)
+						}
+						return ''
 					})
 				})}
 			</div>
@@ -148,59 +257,28 @@ const ContentGrid = ({ hideHeader, table }) => {
 					return items.map((item, columnIndex) => {
 						return (
 							<div
-								key={rowIndex + columnIndex}
-								data-rows={tableJSON.data.length}
-								data-row-number={rowIndex}
-								data-columns={tableJSON.columns.length}
-								data-column-number={columnIndex}
 								css={
 									item.isColumn === true ? columnCss : rowCss
 								}
-								dangerouslySetInnerHTML={{ __html: item.value }}
+								dangerouslySetInnerHTML={{
+									__html: item.value,
+								}}
+								data-grid={`${
+									hideHeader ? 'hide-header' : 'show-header'
+								} ${
+									item.isColumn ? 'column' : 'row'
+								} ${rowIndex}-${columnIndex} ${
+									hideHeader ? 'hide-header' : 'show-header'
+								} ${item.display ? item.display : ''} ${
+									item.hasLastValue === true ? 'last' : ''
+								}`}
+								key={rowIndex + columnIndex}
 							></div>
 						)
 					})
 				})}
 			</div>
-			<div css={tableCss}>
-				<table>
-					<thead>
-						<tr>
-							<th>Review Cycle</th>
-							<th>Letter of Intent Due</th>
-							<th>Due Date</th>
-							<th>Review Dates</th>
-							<th>Funding Decisions</th>
-							<th>Earliest Start Date</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>
-								Innovation Grants (six categories: see below)
-							</td>
-							<td>
-								None
-								<br />* Excluding Linked Standard Research
-								Grants (see below)
-							</td>
-							<td>Nov. 15, 2021</td>
-							<td>Spring 2022</td>
-							<td>End of May 2022</td>
-							<td>October 2022</td>
-						</tr>
-						<tr>
-							<td>Focus Grants</td>
-							<td>Aug. 1, 2021, 11:59 p.m. EDT</td>
-							<td>Dec. 7, 2021</td>
-							<td>Spring 2022</td>
-							<td>End of May 2022</td>
-							<td>October 2022</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-		</div>
+		</>
 	)
 }
 
