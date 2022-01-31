5ab2d335c0299b1c8398cdc0d31aa8b7a54e0049
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2022-01-31 17:37:57 -0500

    completed styling of sbtc grid

--- src/components/Search/SearchHitsSBTC.js
+++ src/components/Search/SearchHitsSBTC.js
@@ -5,50 +5,8 @@ import ContentGrid from '../Content/ContentGrid'
 
 import { styles } from '../../css/css'
 
-const hitCSS = css`
-	display: grid;
-	grid-template-columns: 0.6fr 1.4fr;
-	grid-template-rows: 1fr;
-	grid-gap: ${styles.gridGap.mobile};
-	align-items: stretch;
-	border: ${styles.colors.darkGray} solid ${styles.scale.px2};
-	border-radius: ${styles.scale.px5};
-	min-height: ${styles.scale.px46};
-	img {
-		display: inline-block;
-		margin: 0;
-		object-fit: cover;
-	}
-	p {
-		padding: ${styles.scale.px16};
-		color: ${styles.colors.poppy};
-	}
-	@media (min-width: ${styles.screens.tablet}px) and (max-width: ${styles
-			.screens.video}px) {
-		grid-template-columns: 1fr;
-		grid-template-rows: 1.5fr 0.5fr;
-	}
-`
-
 const buttonCSS = css`
 	margin: 0;
-`
-
-const videoWrapperCSS = css`
-	background-size: cover;
-	background-position: center;
-	border-radius: ${styles.scale.px7} / ${styles.scale.px5};
-	padding: 56.25% 0 0 0;
-	position: relative;
-	iframe {
-		position: absolute;
-		top: 0;
-		left: 0;
-		bottom: 0;
-		right: 0;
-		border-radius: 5px;
-		border: none;
-	}
 `
 
 const CustomHitsSBTC = data => {
@@ -63,6 +21,7 @@ const CustomHitsSBTC = data => {
 		'Telehealth',
 		'Specialties',
 	]
+
 	data.hits.forEach(hit => {
 		clinicianDataArray.push({
 			Name: hit.name,
@@ -79,20 +38,9 @@ const CustomHitsSBTC = data => {
 			Specialties: hit.specialties,
 		})
 	})
-	// data.hits.forEach(hit => {
-	// 	clinicianDataArray.push({
-	// 		Name: 'foo',
-	// 		Address: 'foo',
-	// 		Email: 'foo',
-	// 		Phone: 'foo',
-	// 		Telehealth: 'foo',
-	// 		Specialties: 'foo',
-	// 	})
-	// })
-	console.log(clinicianHeaderArray)
-	console.log(clinicianDataArray)
+
 	return (
-		<>
+		<div>
 			<Configure hitsPerPage={display} />
 			<ContentGrid
 				table={{
@@ -111,7 +59,7 @@ const CustomHitsSBTC = data => {
 					Load more
 				</button>
 			)}
-		</>
+		</div>
 	)
 }
 
