5412e2518d1b3fb1a3a084df4d7bc00f037d7fd6
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2022-02-11 12:28:19 -0500

    removed commenting from state facts

--- src/pages/state-facts.js
+++ src/pages/state-facts.js
@@ -9,47 +9,47 @@ import FactsListContainer from '../components/StateFacts/FactsListContainer'
 import { styles } from '../css/css'
 
 const StateFacts = ({ data: { stateFactsPage, stateFacts } }) => {
-	// let metaImage,
-	// 	metaDescription = ''
-	// stateFactsPage.seoMetaTags.tags.forEach(tag => {
-	// 	if (tag.attributes) {
-	// 		if (
-	// 			tag.attributes.property &&
-	// 			tag.attributes.property === 'og:image'
-	// 		) {
-	// 			metaImage = tag.attributes.content
-	// 		}
-	// 		if (
-	// 			tag.attributes.property &&
-	// 			tag.attributes.property === 'og:description'
-	// 		) {
-	// 			metaDescription = tag.attributes.content
-	// 		}
-	// 	}
-	// })
-	// const structuredData = {
-	// 	'@content': 'https://schema.org',
-	// 	'@type': 'WebPage',
-	// 	about: 'suicide',
-	// 	description: metaDescription,
-	// 	image: metaImage,
-	// 	accessibilityAPI: 'ARIA',
-	// 	accessibilityControl: ['fullKeyboardControl', 'fullMouseControl'],
-	// 	name: stateFactsPage.title,
-	// 	lastReviewed: stateFactsPage.meta.publishedAt,
-	// 	publisher: 'American Foundation for Suicide Prevention',
-	// 	url: `https://afsp.org/${stateFactsPage.slug}`,
-	// }
-	// return (
-	// 	<Layout
-	// 		theme={styles.logo.mobileLightDesktopLight}
-	// 		seo={stateFactsPage.seoMetaTags}
-	// 		structuredData={structuredData}
-	// 	>
-	// 		<HeroSolid data={stateFactsPage} />
-	// 		<FactsListContainer stateFacts={stateFacts.edges} />
-	// 	</Layout>
-	// )
+	let metaImage,
+		metaDescription = ''
+	stateFactsPage.seoMetaTags.tags.forEach(tag => {
+		if (tag.attributes) {
+			if (
+				tag.attributes.property &&
+				tag.attributes.property === 'og:image'
+			) {
+				metaImage = tag.attributes.content
+			}
+			if (
+				tag.attributes.property &&
+				tag.attributes.property === 'og:description'
+			) {
+				metaDescription = tag.attributes.content
+			}
+		}
+	})
+	const structuredData = {
+		'@content': 'https://schema.org',
+		'@type': 'WebPage',
+		about: 'suicide',
+		description: metaDescription,
+		image: metaImage,
+		accessibilityAPI: 'ARIA',
+		accessibilityControl: ['fullKeyboardControl', 'fullMouseControl'],
+		name: stateFactsPage.title,
+		lastReviewed: stateFactsPage.meta.publishedAt,
+		publisher: 'American Foundation for Suicide Prevention',
+		url: `https://afsp.org/${stateFactsPage.slug}`,
+	}
+	return (
+		<Layout
+			theme={styles.logo.mobileLightDesktopLight}
+			seo={stateFactsPage.seoMetaTags}
+			structuredData={structuredData}
+		>
+			<HeroSolid data={stateFactsPage} />
+			<FactsListContainer stateFacts={stateFacts.edges} />
+		</Layout>
+	)
 }
 
 export default StateFacts
