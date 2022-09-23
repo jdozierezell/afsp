629cb4d9ebc9b368ed7a762f38a455ac209c6688
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2022-09-23 12:35:58 -0400

    removed ssr function

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,17 +1,7 @@
-// replace inline css/scss with links
-exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-	if (process.env.NODE_ENV !== 'production') return
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/ssr-apis/
+ */
 
-	let hc = getHeadComponents()
-	hc.forEach(el => {
-		if (el.type === 'style') {
-			el.type = 'link'
-			el.props['href'] = el.props['data-href']
-			el.props['rel'] = 'stylesheet'
-			el.props['type'] = 'text/css'
-
-			delete el.props['data-href']
-			delete el.props['dangerouslySetInnerHTML']
-		}
-	})
-}
+// You can delete this file if you're not using it
