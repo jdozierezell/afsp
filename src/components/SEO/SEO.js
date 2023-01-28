3fae032bb6f83e8588f8181a3df59cc1373a3099
Jonathan Dozier-Ezell <jdozierezell@gmail.com>
2023-01-27 16:58:20 -0800

    deactivated osano

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -4,7 +4,7 @@ import lifesaver from '../SVGs/Lifesaver.svg'
 export const SEO = ({ meta, structuredData, children }) => {
 	return (
 		<>
-			<script src="https://cmp.osano.com/AzqSHrT3nT9b72sZY/7f1727dd-94d7-47d2-834c-a78ee90e83c7/osano.js"></script>
+			{/* <script src="https://cmp.osano.com/AzqSHrT3nT9b72sZY/7f1727dd-94d7-47d2-834c-a78ee90e83c7/osano.js"></script> */}
 			<script
 				dangerouslySetInnerHTML={{
 					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
